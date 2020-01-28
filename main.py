7d2bc70d67d14eb1e46de92215e91599a08eff57
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-28 04:42:30 +0000

    push

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ DECAY_START     = 100
 BATCH_SIZE      = 1
 ADAM_BETA_1     = 0.5                       # Adam optimizer beta1 (beta2 is always 0.999).
 G_LR            = 0.0002                    # Generator learning rate.
-D_LR            = 0.00005                   # Discriminator learning rate.
+D_LR            = 0.0002                    # Discriminator learning rate.
 ON_CUDA         = torch.cuda.is_available() # Boolean for CUDA availability.
 
 if ON_CUDA:
